4ce49eee1930dd115fc5974baf7631c61e91dfa8
Takashi698 <77352602+Takashi698@users.noreply.github.com>
2021-03-08 18:15:07 +0900

    Add files via upload

--- judgement.js
+++ judgement.js
@@ -17,35 +17,35 @@
 function get_achivement(x, y, z){
   let sum = x+y+z;
   if(sum >=250){
-  return “あなたの成績はAです”;
+  return "あなたの成績はAです";
   }else if(sum >=200){
-  return “あなたの成績はBです”;
+  return "あなたの成績はBです";
   }else if(sum >=150){
-  return “あなたの成績はCです”;
+  return "あなたの成績はCです";
   }else{
-  return “あなたの成績はDです”;
+  return "あなたの成績はDです";
   };
 
   console.log(get_achievement(80,80,80));
 
   function get_pass_or_failure(x, y, z){
   if(x<60){
-  return “不合格”;
+  return "不合格";
   }else if(y<60){
-  return “不合格”;
+  return "不合格";
   }else if(z<60){
-  return “不合格”;
+  return "不合格";
   }else{
-  return “合格”;
+  return "合格";
   }
   };
 
   console.log(get_pass_or_failure(80,80,80));
   
   function judgement(x, y, z){
-    let achievement = get_achivement(x, y, z);
+    let achievement = get_achievement(x, y, z);
     let pass_or_failure = get_pass_or_failure(x, y, z);
     return `あなたの成績は${achievement}です。${pass_or_failure}です！`;
     };
 
-    console.log(judgement(10,10,10));
+    console.log(judgement(10,10,10);
